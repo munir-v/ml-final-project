95febdb5e862e68636827f308a2b01735949f326
munir-v <81745551+munir-v@users.noreply.github.com>
2023-12-04 11:50:12 -0800

    fix: add more rows to cleaned data

--- data_cleaning.py
+++ data_cleaning.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from pandas import json_normalize
 
 hrv1 = pd.read_json("/content/TrainingReadinessDTO_20221128_20230308_107145992.json")
 hrv2 = pd.read_json("/content/TrainingReadinessDTO_20230308_20230616_107145992.json")
@@ -20,7 +21,7 @@ merged_activity_data = pd.concat([activity1, activity2, activity3, activity4])
 merged_activity_data = pd.merge(
     merged_activity_data, merged_hrv_data, on="calendarDate", how="inner"
 )
-# merged_activity_data.to_csv('activity_data.csv', index=False)
+merged_activity_data.to_csv("activity_data.csv", index=False)
 
 pd.set_option("display.max_columns", None)
 data = pd.read_csv("activity_data.csv")
@@ -32,6 +33,10 @@ data = data[
         "calendarDate",
         "totalSteps",
         "highlyActiveSeconds",
+        "activeSeconds",
+        "moderateIntensityMinutes",
+        "minAvgHeartRate",
+        "maxAvgHeartRate",
         "moderateIntensityMinutes",
         "vigorousIntensityMinutes",
         "minHeartRate",
@@ -44,7 +49,15 @@ data = data[
 data.dropna()
 data.to_csv("cleaned_activity_data.csv", index=False)
 
-sleep = pd.read_csv("/content/merged_sleep_data.csv")
+sleep1 = pd.read_json("/content/2023-08-03_2023-11-11_107145992_sleepData.json")
+sleep2 = pd.read_json("/content/2023-01-15_2023-04-25_107145992_sleepData.json")
+sleep3 = pd.read_json("/content/2023-04-25_2023-08-03_107145992_sleepData.json")
+sleep4 = pd.read_json("/content/2022-10-07_2023-01-15_107145992_sleepData.json")
+merged_sleep_data = pd.concat([sleep1, sleep2, sleep3, sleep4])
+
+sleep_scores_expanded = json_normalize(merged_sleep_data["sleepScores"])
+merged_sleep_data = merged_sleep_data.drop("sleepScores", axis=1)
+sleep = merged_sleep_data.join(sleep_scores_expanded)
 sleep.head()
 
 sleep["deepSleepSeconds"] = pd.to_timedelta(sleep["deepSleepSeconds"], unit="S")
@@ -73,6 +86,12 @@ sleep = sleep[
         "totalSleepHours",
         "overallScore",
         "qualityScore",
+        "averageRespiration",
+        "lowestRespiration",
+        "highestRespiration",
+        "awakeCount",
+        "avgSleepStress",
+        "restlessMomentCount",
     ]
 ]
 
@@ -80,5 +99,5 @@ sleep.to_csv("cleaned_activity_data.csv", index=False)
 
 all_data = pd.merge(data, sleep, on="calendarDate", how="inner")
 all_data.dropna()
-all_data.to_csv('final_data.csv', index=False)
-
+all_data.to_csv("final_data.csv", index=False)
+# all_data
